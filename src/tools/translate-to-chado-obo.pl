966f0826cf139396766f48c7ea51754dd09915cf
Chris Mungall <cjm@berkeleybop.org>
2015-12-17 07:12:21 -0800

    Making labels underscore separated, adding a default namespace tag, adding a fake OBO_REL:is_a. Issue #68

---
+++ src/tools/translate-to-chado-obo.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+use strict;
+my $last;
+while(<>) {
+    chomp;
+    next if m@^owl-axioms@;
+    if (m@name: (.*)@) {
+        my $n = $1;
+        $n =~ s@\s+@_@g;
+        $_ = "name: $n";
+    }
+    if (m@^ontology@) {
+        print "default-namespace: ro\n";
+    }
+    $last = $_;
+    print "$_\n";
+}
+print "\n" unless $last =~ m@\s*@;
+print "[Typedef]\n";
+print "id: OBO_REL:is_a\n";
+print "name: is_a\n";
+print "comment: This relation is added for backward compatibility with Chado\n\n";
